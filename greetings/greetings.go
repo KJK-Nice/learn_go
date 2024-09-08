6e95e183540e0b0cb4f097160f298707b0cdba3b
khajornkiat usap <khajornkiat.us@ols.co.th>
2024-09-08 22:16:09 +0700

    Return greetings for multiple people

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -1,9 +1,8 @@
 package greetings
 
 import (
-    "fmt"
     "errors"
-
+    "fmt"
     "math/rand"
 )
 
@@ -14,6 +13,18 @@ func Hello(name string) (string, error) {
     }
     message := fmt.Sprintf(randomFormat(), name)
     return message, nil
+}
+
+func Hellos(names []string) (map[string]string, error) {
+    messages := make(map[string]string)
+    for _, name := range names {
+        message, err := Hello(name)
+        if err != nil {
+            return nil, err
+        }
+        messages[name] = message
+    }
+    return messages, nil
 }
 
 func randomFormat() string {
--- hello/hello.go
+++ hello/hello.go
@@ -15,8 +15,10 @@ func main() {
     log.SetPrefix("greetings: ")
     log.SetFlags(0)
 
+    names := []string{"Gladys", "Samantha", "Darrin"}
+
     // Request a greeting message.
-    message, err := greetings.Hello("Nice")
+    messages, err := greetings.Hellos(names)
     // If an error was returned, print it to the console and
     // exit the program.
     if err != nil {
@@ -25,6 +27,6 @@ func main() {
 
     // If no error was returned, print the returned message
     // to the console.
-    fmt.Println(message)
+    fmt.Println(messages)
     fmt.Println(quote.Go())
 }
